939dd0d68bdb213f8a089a8cb32e7cb7d0796cdf
David <74268782+DrOgres@users.noreply.github.com>
2023-06-07 11:27:55 -0500

    start of css and character sheet data loading

--- module/twdu.js
+++ module/twdu.js
@@ -2,7 +2,7 @@ import TWDUItemSheet from "./sheet/TWDUItemSheet.js";
 import TWDUActorSheet from "./sheet/TWDUActorSheet.js";
 import {twdu} from "../module/config.js";
 
-Hooks.once("init", ()=> {
+Hooks.once('init', async function () {
     console.log("TWDU | Initializing TWDU");
 
     CONFIG.twdu = twdu;
@@ -12,6 +12,20 @@ Hooks.once("init", ()=> {
 
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("twdu", TWDUActorSheet, { makeDefault: true });
+
+      // Preload Handlebars Templates
+ //  preloadHandlebarsTemplates(); define in templates.js once we have our tabs defined
+
+  Handlebars.registerHelper('TWDUconcat', function () {
+    console.log('TWDU | TWDUconcat');
+    let outStr = '';
+    for (let arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
     
 });
     
