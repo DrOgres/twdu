7131200f72482fa168f893cc5d1f20a41dd6a28c
David <74268782+DrOgres@users.noreply.github.com>
2023-06-10 20:10:38 -0500

    editor

--- module/twdu.js
+++ module/twdu.js
@@ -1,49 +1,72 @@
 import TWDUItemSheet from "./sheet/TWDUItemSheet.js";
 import TWDUActorSheet from "./sheet/TWDUActorSheet.js";
 import { preloadHandlebarsTemplates } from "./util/templates.js";
-import {twdu} from "../module/config.js";
+import { twdu } from "../module/config.js";
 
+Hooks.once("init", async function () {
+  console.log("TWDU | Initializing TWDU");
 
-Hooks.once('init', async function () {
-    console.log("TWDU | Initializing TWDU");
+  CONFIG.twdu = twdu;
 
-    CONFIG.twdu = twdu;
+  CONFIG.TextEditor.enrichers = CONFIG.TextEditor.enrichers.concat([
+    {
+      pattern: /@RAW\[(.+?)\]/gm,
+      enricher: async (match, options) => {
+        const myData = await $.ajax({
+          url: match[1],
+          type: "GET",
+        });
+        const doc = document.createElement("span");
+        doc.innerHTML = myData;
+        return doc;
+      },
+    },
+  ]);
 
-    Items.unregisterSheet("core", ItemSheet);
-    Items.registerSheet("twdu", TWDUItemSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("twdu", TWDUItemSheet, { makeDefault: true });
 
-    Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("twdu", TWDUActorSheet, { makeDefault: true });
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("twdu", TWDUActorSheet, { makeDefault: true });
 
-      // Preload Handlebars Templates
+  // Preload Handlebars Templates
   preloadHandlebarsTemplates();
 
-  Handlebars.registerHelper('TWDUconcat', function () {
-    console.log('TWDU | TWDUconcat: ' + arguments[0] + ' : ' + arguments[1]);
-    let outStr = '';
+  Handlebars.registerHelper("enrichHtmlHelper", function (rawText) {
+    return TextEditor.enrichHTML(rawText, { async: false });
+  });
+
+  Handlebars.registerHelper("TWDUconcat", function () {
+    console.log("TWDU | TWDUconcat: " + arguments[0] + " : " + arguments[1]);
+    let outStr = "";
     for (let arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
+      if (typeof arguments[arg] != "object") {
         outStr += arguments[arg];
       }
     }
     return outStr;
   });
 
-  Handlebars.registerHelper('skillBlock', function () {
-    console.log('TWDU | skillBlock ' + arguments[0] + ' : ' + arguments[1]);
+  Handlebars.registerHelper("skillBlock", function () {
+    console.log("TWDU | skillBlock " + arguments[0] + " : " + arguments[1]);
     let arg = arguments[0];
-    let outStr = '';
-    for(let skill in arguments[1]) {
+    let outStr = "";
+    for (let skill in arguments[1]) {
       if (arguments[1][skill].attribute == arg) {
-        console.log('TWDU | skillBlock found ' + arg + ' : ' + arguments[1][skill].label);
-        outStr += `<div>`+ game.i18n.localize('twdu.'+arguments[1][skill].label)+`</div> <input type="text" name="system.skills.`+arguments[1][skill].name+`" value="`+arguments[1][skill].value+`">`;
+        console.log(
+          "TWDU | skillBlock found " + arg + " : " + arguments[1][skill].label
+        );
+        outStr +=
+          `<div>` +
+          game.i18n.localize("twdu." + arguments[1][skill].label) +
+          `</div> <input type="text" name="system.skills.` +
+          arguments[1][skill].name +
+          `" value="` +
+          arguments[1][skill].value +
+          `">`;
       }
     }
-  
+
     return outStr;
   });
-    
-
-    
 });
-    
